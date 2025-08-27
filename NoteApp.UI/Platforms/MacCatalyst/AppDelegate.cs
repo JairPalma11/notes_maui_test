c9f2277651231aeacee363aafa178dc170444695
Jair Palma <jpalma.teachme@gmail.com>
2025-08-27 15:46:12 -0400

    init

---
+++ NoteApp.UI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NoteApp.UI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
